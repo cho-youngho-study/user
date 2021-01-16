0120ea38654327d270557677753cb062662e283b
ChoYoungHo <choyoungho95@gmail.com>
2021-01-16 21:27:59 +0900

    cors이슈 및 proxy 설정

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const LandingPage = () => {
     useEffect(()=>{
-        axios.get('http://localhost:5000/api/hello').then(response => console.log(response.data))
+        axios.get('/api/hello').then(response => console.log(response))
     }, [])
 
 
